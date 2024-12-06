a0de6f842e4a64845af5a1f7277af63889630991
stuartm <stuartm.rec@gmail.com>
2024-12-06 18:45:34 +0000

    Pick a random choice for computer

--- rps_project.js
+++ rps_project.js
@@ -1,1 +1,9 @@
-console.log("Weeeee!");
+let options = ["Rock", "Paper", "Scissors"];
+
+let result = options[getComputerChoice()];
+console.log(result);
+
+function getComputerChoice(){
+    let x = Math.random() * 3;
+    return Math.floor(x);
+}
